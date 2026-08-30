758d64ef0807aad57a24d4bbdfb1e711716ff12a
Dhruv Chauhan <dhruvchauhan1489@gmail.com>
2026-08-30 14:44:31 +0530

    Initial Commit

---
+++ AirWhisperTrain/AirWhisperTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirWhisperTrainApp.swift
+//  AirWhisperTrain
+//
+//  Created by Dhruv Chauhan on 30/08/26.
+//
+
+import SwiftUI
+
+@main
+struct AirWhisperTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirWhisperTrain/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AirWhisperTrain
+//
+//  Created by Dhruv Chauhan on 30/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
